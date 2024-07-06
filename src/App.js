46e9ceec9e9ad007a9e21deb447c017a8bb3d177
Ammaar-nalkhandE <ammaar.nalkhande@gmail.com>
2024-07-06 23:54:58 +0530

    basic layout of home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar'; // Adjust the path as needed
+import HomePage from './pages/homepage';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <HomePage/>
       {/* Other components go here */}
     </div>
   );
