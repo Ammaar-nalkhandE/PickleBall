a38aa8309ea50951e2e0461d743a53620bccf5fc
Ammaar-nalkhandE <ammaar.nalkhande@gmail.com>
2024-07-07 11:08:31 +0530

    basic about page

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our website! We are dedicated to providing the best service.
+        Our team works hard to ensure customer satisfaction.
+      </p>
+      <p>
+        Our mission is to deliver high-quality products and services that bring value to our customers.
+      </p>
+    </div>
+  );
+};
+
+export default About;
